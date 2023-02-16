9a6113d71bdd8c5acc825166d83be4479d5054ea
Gustavo Guichard <gustavoguichard@gmail.com>
2023-02-16 15:21:36 -0300

    Prepare the talk and add some tests

--- backend-for-frontend/app/domain/purchases.server.ts
+++ backend-for-frontend/app/domain/purchases.server.ts
@@ -1,14 +1,15 @@
-import { faker } from "@faker-js/faker";
-import { makeDomainFunction } from "domain-functions";
+import { faker } from '@faker-js/faker'
+import { makeDomainFunction } from 'domain-functions'
 import * as z from 'zod'
 
 export const getPurchases = makeDomainFunction(
-    z.object({ quantity: z.coerce.number().default(10) })
+  z.object({ quantity: z.coerce.number().default(10) }),
 )(async ({ quantity }) => {
   const purchases = Array.from({ length: Number(quantity) }, () => ({
     number: faker.datatype.number({ min: 1256, max: 4589 }),
-    href: "#",
-    invoiceHref: "#",
+    href: '#',
+    invoiceHref: '#',
+    // userId: Math.ceil(Math.random() * 6),
     createdDate: faker.date.recent(),
     createdDatetime: faker.date.recent(),
     deliveredDate: faker.date.recent(),
@@ -25,11 +26,11 @@ export const getPurchases = makeDomainFunction(
         id: faker.datatype.uuid(),
         name: faker.commerce.productName(),
         description: faker.commerce.productDescription(),
-        href: "#",
+        href: '#',
         price: `$${faker.commerce.price()}`,
         imageSrc: faker.image.abstract(),
         imageAlt: faker.lorem.sentence(),
-      })
+      }),
     ),
   }))
 
--- my-store/src/api/store.api.ts
+++ my-store/src/api/store.api.ts
@@ -1,5 +1,4 @@
-import { makeDomainFunction } from 'domain-functions';
-import { z } from "zod";
+import { z } from 'zod'
 
 const productSchema = z.object({
   id: z.string().min(1),
@@ -9,7 +8,7 @@ const productSchema = z.object({
   price: z.string().min(1),
   imageSrc: z.string().min(1),
   imageAlt: z.string().min(1),
-});
+})
 
 const purchaseSchema = z.object({
   number: z.number().min(4),
@@ -21,18 +20,36 @@ const purchaseSchema = z.object({
   deliveredDatetime: z.string().min(1),
   total: z.string().min(1),
   products: z.array(productSchema),
-});
-
-const schema = z.array(purchaseSchema);
-
-export type Purchase = z.infer<typeof purchaseSchema>;
-
-export type Product = z.infer<typeof productSchema>;
-
-export const getPurchases = makeDomainFunction(z.coerce.number().default(10))(async (quantity) => {
-  const { purchases } = await fetch(
-    `http://localhost:3001/purchases?quantity=${quantity}`
-  ).then((res) => res.json());
-
-  return schema.parse(purchases);
 })
+
+export type Purchase = z.infer<typeof purchaseSchema>
+
+export const getPurchases = async (quantity = 10) => {
+  const { purchases } = await fetch(
+    `http://localhost:3001/purchases?quantity=${quantity}`,
+  ).then((res) => res.json())
+
+  // safe?
+
+  return z.array(purchaseSchema).safeParse(purchases)
+}
+
+// const userSchema = z.object({
+//   id: z.number(),
+//   email: z.string().email(),
+//   first_name: z.string().min(2),
+//   last_name: z.string().min(2),
+//   avatar: z.string().url(),
+// })
+
+// export type User = z.infer<typeof userSchema>
+
+// export const getUsers = makeDomainFunction()(async () => {
+//   const { data } = await fetch('https://reqres.in/api/users').then((res) =>
+//     res.json(),
+//   )
+
+//   return {
+//     users: z.array(userSchema).parse(data),
+//   }
+// })
